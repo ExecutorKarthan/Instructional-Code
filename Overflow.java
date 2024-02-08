e94c192eb73ce685e4cf7c045b4d0559f3319418
Alex Messina <120221657+ExecutorKarthan@users.noreply.github.com>
2024-02-08 13:11:24 -0600

    Overflow Example

---
+++ Overflow.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args){
+        int count = 0;
+        int value = 2147483643;
+        System.out.println(value);
+        while (count < 10){
+            count = count +1;
+            value = value + 1;
+            System.out.println(value);
+        }
+    }    
+}
